6e52b22a24c11cb6acde4690557a1d1e0c1574f8
chimi <ckzgraphics@users.noreply.github.com>
2019-07-29 20:27:46 +0530

    Update spec0.js

--- test/specs/spec0.js
+++ test/specs/spec0.js
@@ -1,22 +1,6 @@
 const assert = require('assert');
 
-describe('webdriver_io_page', () => {
-    
-    it('should have the right title', () => {
-        browser.url('https://webdriver.io');
-        const title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
-    });
-    
-    it('Open Google URL', () => {
-        browser.url('https://www.google.com');
-        assert.equal('Hello', 'Hello');
-    });
-    
-});
-
-
-describe('New_Facebook_Test', () => {
+describe('New.Facebook Test', () => {
     
     it('Open Facebook', () => {
         browser.url('https://www.facebook.com');
