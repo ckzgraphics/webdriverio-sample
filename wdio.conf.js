9dd3c2cd1c9d26cfa25dcec5cf6c5e342044dd37
chimi <ckzgraphics@users.noreply.github.com>
2019-08-08 20:30:21 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -29,8 +29,8 @@ exports.config = {
     // should work too though). These services define specific user and key (or access key)
     // values you need to put in here in order to connect to these services.
     //
-    user: process.env.BROWSERSTACK_USER,
-    key: process.env.BROWSERSTACK_ACCESSKEY,
+    user: process.env.BROWSERSTACK_USERNAME,
+    key: process.env.BROWSERSTACK_ACCESS_KEY,
     //
     // If you run your tests on SauceLabs you can specify the region you want to run your tests
     // in via the `region` property. Available short handles for regions are `us` (default) and `eu`.
