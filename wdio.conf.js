b6cd8a6f7ba7aa1a6441de589f8b5af5fd411b74
chimi <ckzgraphics@users.noreply.github.com>
2019-08-08 20:07:20 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -63,7 +63,8 @@ exports.config = {
     // order to group specific specs to a specific capability.
     //
     // First, you can define how many instances should be started at the same time. Let's
-    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
+    // say you have 3 different capabilities (Chrome, 
+    , and Safari) and you have
     // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
     // files and you set maxInstances to 10, all spec files will get tested at the same time
     // and 30 processes will get spawned. The property handles how many capabilities
@@ -81,7 +82,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'firefox',
+        browserName: 'chrome',
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
